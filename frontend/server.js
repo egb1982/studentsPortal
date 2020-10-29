c43af7804320a2ba8e119fa15c8c2ca0119dcd10
Emilio <egb1982@gmail.com>
2020-10-29 18:14:46 +0100

    error

--- frontend/server.js
+++ frontend/server.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.static('./dist/frontend'));
 app.get('/*',(req,res)=>{
-    res.sendFiler('index.html', {root:'dist/frontend/'});
+    res.sendFile('index.html', {root:'dist/frontend/'});
 });
 
 app.listen(process.env.PORT || 8080);
